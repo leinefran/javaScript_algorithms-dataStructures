1b7f39a76712af837c7c43ff87109fdccfb3d4c1
leinefran <penny.leine@gmail.com>
2019-06-15 23:21:01 +0000

    a function that calculates the sum of all numbers from 1 up to (and including) some number n.

---
+++ 1-addUpTo.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// a function that calculates the sum of all numbers from 1 up to # n.
+
+function addUpTo(n) {
+    let total = 0;
+    for (let i = 1; i <= n; i++) {
+	total += 1;
+    }
+    return total;
+}
